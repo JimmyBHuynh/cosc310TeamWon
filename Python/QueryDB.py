f87fc5659a53a808465c2b58720951270da872e1
Robert Teodoropol <robert.teodoropol@gmail.com>
2018-03-04 16:27:15 -0800

    Added support for making DBs

--- Python/QueryDB.py
+++ Python/QueryDB.py
@@ -9,17 +9,30 @@ class HelloWorldExample(object):
     def close(self):
         self._driver.close()
 
-    def print_greeting(self, message):
+    def run_Query(self, query):
         with self._driver.session() as session:
-            greeting = session.write_transaction(self._create_and_return_greeting, message)
-            print(greeting)
+            session.write_transaction(self._create_query, query)
+
+
+    def run_Return_Query(self, query):
+        with self._driver.session() as session:
+            result = session.write_transaction(self._create_and_return_query, query)
+            print(result)  
 
     @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
-                        "SET a.message = $message "
-                        "RETURN a.message + ', from node ' + id(a)", message=message)
+    def _create_query(tx, query):
+        tx.run(query)
+
+    @staticmethod
+    def _create_and_return_query(tx, query):
+        result = tx.run(query)
         return result.single()[0]
 
 test = HelloWorldExample("bolt://localhost:7687","neo4j","test");
-test.print_greeting("hello");
+
+#test.run_Query("CREATE (TheMatrix:Movie {title:'The Matrix', released:1999, tagline:'Welcome to the Real World'}) " );
+test.run_Query("MATCH (n) DETACH DELETE n ");
+
+data = "CREATE (Canucks:Team {Name:'Canucks', Season:'2017/2018', gamesPlayed:65, wins:24, losses:32 , ties:0}) CREATE (Flames:Team {Name: 'Flames',Season:'2017/2018', gamesPlayed:66, wins:32, losses:25 , ties:0}) CREATE (MapleLeafs:Team {Name: 'Maple Leafs', Season:'2017/2018', gamesPlayed:67, wins:39, losses:21 , ties:0}) aaCREATE (Leipsic:Player {Name:'Brendan Leipsic', gamesPlayed:2, goals:0, assists:2 }) CREATE (Vanek:Player {Name:'Thomas Vanek', gamesPlayed:61, goals:17, assists:24 }) CREATE (Leipsic:Player {Name:'Alex Biega', gamesPlayed:31, goals:0, assists:6 })");
+ test.run_Query(data);
+
