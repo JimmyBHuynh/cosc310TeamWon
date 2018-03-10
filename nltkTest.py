b2d09efc25c02744ae3dc4ad310f6fa998e710dc
dtmle <35821286+dtmle@users.noreply.github.com>
2018-03-09 18:08:55 -0800

    Add files via upload
    added iter
    - still need better method to go through all words in string while keeping track of states!!!!!!!!

--- nltkTest.py
+++ nltkTest.py
@@ -12,10 +12,13 @@ lem = WordNetLemmatizer()
 
 
 #Get input --> Going to be function in itself later
+    
 phrase = input( "Query: " )
 tokens = nltk.word_tokenize( phrase.lower() )
 
 tag = nltk.pos_tag( tokens )
+
+iterTag = iter( tag )
 
 print( tag )
 
@@ -23,44 +26,43 @@ query = ""
 
 
 #Right now, check if first word is our keyword get
-if tag[0][0] == "get":
+if next( iterTag )[0] == "get":
 
-    nTag = tag[1][1]
-
+    nTag = next( iterTag, None )
 
     #Need to implement if they use words like all
-    if tag[1][0] == "DT":
+    if nTag[1] == "DT":
 
         print( "determinant" )
         
     #if not, we can check if they are trying to get a plural noun
-    elif nTag == "NNS" or nTag == "NNPS":
+    elif nTag[1] == "NNS" or nTag[1] == "NNPS":
 
         #print( "noun plural" )
-        label = tag[1][0]
+        label = nTag[0]
 
         #If it is a plural noun, and thats all there is to the string
         #Then its match label
         if len( tag ) == 2:
 
-            var = tag[1][0][0]
+            var = nTag[0][0]
             #print( var )
 
             query = "Match(" + var + " :" + label + ")" + "\n" + "RETURN " + var
 
         else:
         
-            nTag = tag[2][1]
+            nTag = next( iterTag )
 
             #If third word is tagged in, perhaps its a property????
-            if nTag == "IN":
+            if nTag[1] == "IN":
 
                 var = tag[1][0][0]
                 prop = lem.lemmatize( tag[1][0] )
-                nTag = tag[3][1]
+                nTag = next( iterTag, None )
 
                 #If fourth word is noun, then its match property of label
-                if nTag == "NNS" or nTag == "NNPS":
+                if nTag[1] == "NNS" or nTag[1] == "NNPS":
 
                     label = lem.lemmatize( tag[3][0] )
                     label = label[0].upper() + label[1:]
