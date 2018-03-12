a4a92f5db90049be98a832e14c08a5bced830026
dtmle <35821286+dtmle@users.noreply.github.com>
2018-03-12 15:15:32 -0700

    added loop to go through string
    gets rid of useless text before keyworsd

--- nltkTest.py
+++ nltkTest.py
@@ -1,6 +1,5 @@
 import nltk
-from nltk.corpus import treebank
-#Inflect engine faster than lemmatizer for singularizing words
+from nltk.corpus import treebank #Inflect engine faster than lemmatizer for singularizing words
 #import inflect
 from nltk.stem import WordNetLemmatizer
 
@@ -9,6 +8,8 @@ lem = WordNetLemmatizer()
 
 #Inflect engine
 #engine = inflect.engine()
+
+keywords = "get", "count", "number", "of", "where"
 
 
 #Get input --> Going to be function in itself later
@@ -24,53 +25,91 @@ print( tag )
 
 query = ""
 
+count = 0
+#Needto: add a while( next(iterTag)[0] != "get" before here
+
+while next( iterTag )[ 0 ] != "get":
+
+    count = count + 1
+
 
 #Right now, check if first word is our keyword get
-if next( iterTag )[0] == "get":
+count = count + 1
+nTag = next( iterTag, None )
 
-    nTag = next( iterTag, None )
+#print( str( count ) )
+#print( len( tag ) )
 
     #Need to implement if they use words like all
-    if nTag[1] == "DT":
+if nTag[1] == "DT":
 
-        print( "determinant" )
+    print( "determinant" )
         
-    #if not, we can check if they are trying to get a plural noun
-    elif nTag[1] == "NNS" or nTag[1] == "NNPS":
+#if not, we can check if they are trying to get a plural noun
 
-        #print( "noun plural" )
-        label = nTag[0]
+    #Sometimes a word is put after Get that is not a noun.
+    #Need to handle that case before here
 
-        #If it is a plural noun, and thats all there is to the string
-        #Then its match label
-        if len( tag ) == 2:
 
-            var = nTag[0][0]
-            #print( var )
+elif nTag[1] == "NNS" or nTag[1] == "NNPS":
 
-            query = "Match(" + var + " :" + label + ")" + "\n" + "RETURN " + var
+    #print( "noun plural" )
+    label = lem.lemmatize( nTag[0] )
+    count = count + 1
 
-        else:
-        
-            nTag = next( iterTag )
+    #If it is a plural noun, and thats all there is to the string
+    #Then its match label
+    if len( tag ) == count:
 
-            #If third word is tagged in, perhaps its a property????
-            if nTag[1] == "IN":
+        var = nTag[0][0]
+        #print( var )
 
-                var = tag[1][0][0]
-                prop = lem.lemmatize( tag[1][0] )
-                nTag = next( iterTag, None )
+        query = "Match(" + var + " :" + label + ")" + "\n" + "RETURN " + var
 
-                #If fourth word is noun, then its match property of label
-                if nTag[1] == "NNS" or nTag[1] == "NNPS":
+    else:
 
-                    label = lem.lemmatize( tag[3][0] )
-                    label = label[0].upper() + label[1:]
+        #ADD WHILE LOOP HERE LOOKING FOR MORE KEYWORDS - NUMBER OF, COUNT OF
+        """
 
-                    #Nothing else
-                    if len( tag ) == 4:
+        dictionary.contains( currWord )?
+        if contains ---- check tag of currWord
+        do as required for tag
+
+
+        AFTER YOU MATCH GET, CHECK NEXT WORD!
+
+        To look for: count of, number of, of, where, starts with, ends with, contains
+            is null
+            is not null
+            distinct
+
+            ?? PARENTS OF??? <--- will get matched as a property with current implementation
+
+            Get (prop)___ where name is Joe and is a (label)______ of? 
+
+
+        """
+        nTag = next( iterTag )
+        count = count + 1
+        #If third word is tagged in, perhaps its a property????
+        if nTag[1] == "IN":
+
+            var = tag[count][0][0]
+            #PLURALS SOMETIMES DONT WORK --- NOTE WORDS ENDING IN "IES"
+            prop = lem.lemmatize( tag[count-2][0] )
+            nTag = next( iterTag, None )
+            count = count + 1
+
+            #If fourth word is noun, then its match property of label
+            if nTag[1] == "NNS" or nTag[1] == "NNPS":
+
+               label = lem.lemmatize( tag[count-1][0] )
+               label = label[0].upper() + label[1:]
+
+               #Nothing else
+            if len( tag ) == count:
    
-                        query = "Match(" + var + " :" + label + ")" + "\n" + "RETURN " + var + "." + prop
+                    query = "Match(" + var + " :" + label + ")" + "\n" + "RETURN " + var + "." + prop
             
         
 print( query )
