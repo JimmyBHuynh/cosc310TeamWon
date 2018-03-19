22043dcccfec670d31be03507a0de4c17cd13ddc
dtmle <35821286+dtmle@users.noreply.github.com>
2018-03-19 09:12:49 -0700

    conjunction added
    needto: add count.
                 implement josh' part ------ need to fix this code

--- parse.py
+++ parse.py
@@ -7,16 +7,16 @@ from nltk.stem import WordNetLemmatizer
 
 #Lemmatizer
 lem = WordNetLemmatizer()
-
-keywords = "get", "count", "number", "of", "where"
+keywords = "get", "count", "number", "where", "and", "is", "and"
 query = ""
 
 def getInput():
+    
     #Get input --> Going to be function in itself later
         
     phrase = input( "Query: " )
     tokens = nltk.word_tokenize( phrase.lower() )
-
+    
     tag = nltk.pos_tag( tokens )
 
     iterTag = iter( tag )
@@ -34,7 +34,6 @@ def getFirstKeyword( tag ):
     while tag[count][0] not in keywords:
 
         count = count + 1
-        print( "not in keywords: " + str( tag[count][0] ) )
 
     print( "First keyword: " + str( tag[count][0] ) )
            
@@ -53,33 +52,82 @@ def parse( tag, idxKey ):
     elif tag[idxKey + 1][1] == "NNS" or tag[idxKey + 1][1] == "NNPS":
 
         idxKey = idxKey + 1
+        prop = lem.lemmatize( tag[idxKey][0] )
+        var = tag[idxKey][0][0]
 
         #Plural noun handling
         if idxKey + 1 == len( tag ):
 
             var = tag[idxKey][0][0]
             query = "Match(" + var + " :" + tag[idxKey][0] + ")" + "\n" + "RETURN " + var
-
+            
+            
         else:
 
-            #Dunno, let'sl ook for next keyword
+            #Dunno, let's look for next keyword
             print( "looking for next keyword" )
             idxKey = nextKeyword( tag, idxKey )
-            
+            queryPart = handleKeyword( tag, idxKey )
+            query = "Match(" + var + " :" + queryPart + ")" + "\n" + "RETURN " + var + "." + prop
+    else:
 
+        idxKey = idxKey + 1
+        print( "outter loop. Looking for next keyword: " )
+        idxKey = nextKeyword( tag, idxKey )
+            
     print( query )
         
     return
 
 def nextKeyword( tag, idxKey ):
 
-    while tag[idxKey][0] not in keywords:
+    while tag[idxKey][0] not in keywords and idxKey < len( tag )-1:
 
         idxKey = idxKey + 1
 
     print( "Next Keyword: " + tag[idxKey][0] )
 
     return idxKey
+
+def handleKeyword( tag, idxKey ):
+
+    keyword = tag[idxKey][0]
+    keyTag = tag[idxKey][1]
+    queryPart = ""
+
+
+    #bunch of ifs yo
+    if keyword == "count":
+
+        print()
+        
+    elif keyword == "where":
+
+        print()
+
+    elif keyword == "and":
+
+        print()
+        bef = tag[idxKey - 1][0]
+        befTag = tag[idxKey - 1][1]
+        aft = tag[idxKey + 1][0]
+        aftTag = tag[idxKey + 1][0]
+        
+        if befTag == "NNS" or befTag == "NNPS":
+            bef = lem.lemmatize( bef )
+        if aftTag == "NNS" or aftTag == "NNPS":
+            aft = lem.lemmatize( aft )
+        
+        #If plural - singularize
+        #Label conjunction if we find 'and'?
+        queryPart = bef + " :" + aft
+
+    elif keyTag == "NNPS" or keyTag == "NNS":
+
+        queryPart = lem.lemmatize( tag[idxKey][0] )
+        
+
+    return queryPart
 
 tag = getInput()
 idxKey = getFirstKeyword( tag )
